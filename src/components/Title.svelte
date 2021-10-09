eda3332c3e4e61c790abc65be14d8ed032d84536
cz.lee <cz.lee@chelpis.com>
2021-10-09 22:28:28 +0800

    add Title

---
+++ src/components/Title.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let title = ''
+  export let color = ''
+</script>
+
+<h1 style="color: {color}">
+  {title}
+</h1>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
